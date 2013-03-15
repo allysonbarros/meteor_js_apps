972b28e8b4b54b8ae43ab652e755bfb82d710863
Allyson Barros <allysonbarrosrn@gmail.com>
2013-03-15 18:26:44 -0300

    Adicionando formulário de inclusão de novas categorias.

--- LendLib/LendLib.js
+++ LendLib/LendLib.js
@@ -1,15 +1,29 @@
+var lists = new Meteor.Collection("Lists");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to LendLib.";
+  Template.categories.lists = function() {
+    return lists.find({}, {sort: {Category: 1}});
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+  Session.set('adding_category', false);
+
+  Template.categories.new_cat = function() {
+  	return Session.equals('adding_category', true);
+  };
+
+  Template.categories.events({
+  	'click #btnNewCat': function(e, t) {
+  		Session.set('adding_category', true);
+
+  		Meteor.flush();
+  		focusText(t.find("#add-category"));
+  	}
   });
+
+  function focusText(i) {
+		i.focus();
+		i.select();
+	};
 }
 
 if (Meteor.isServer) {
